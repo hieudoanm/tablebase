de8e145274c2f4f6da9c4329d71462153f3c85ab
hieudoanm <hieumdoan@gmail.com>
2022-10-26 23:04:54 +0700

    Quotes - Use Python instead of TypeScript

---
+++ scripts/quotes.py
@@ -0,0 +1,32 @@
+"""
+Get Quotes
+"""
+
+import csv
+import requests
+
+BASE_URL = "https://api.quotable.io/quotes?page={0}&limit=150"
+
+response = requests.get(BASE_URL.format(1))
+responseJSON = response.json()
+totalPages = responseJSON.get('totalPages')
+totalCount = responseJSON.get('totalCount')
+print(totalPages, totalCount)
+
+quotes = []
+
+for i in range(totalPages + 1):
+    quotesResponse = requests.get(BASE_URL.format(i))
+    quotesJSON = quotesResponse.json()
+    quotesResults = quotesJSON.get('results')
+    quotes += quotesResults
+    print(i)
+
+print(len(quotes))
+
+keys = quotes[0].keys()
+
+with open('./data/quotes/quotes.csv', 'w', newline='', encoding='utf-8') as output_file:
+    dict_writer = csv.DictWriter(output_file, keys)
+    dict_writer.writeheader()
+    dict_writer.writerows(quotes)
