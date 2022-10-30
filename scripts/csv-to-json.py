64c911354f9de8103f0e0eb78d9eec4fac476c73
hieudoanm <hieumdoan@gmail.com>
2022-10-30 16:28:43 +0700

    CSV to JSON - TypeScript to Python

---
+++ scripts/csv-to-json.py
@@ -0,0 +1,32 @@
+"""
+CSV to JSON
+"""
+
+import csv
+import json
+import os
+
+def csv_to_json(csv_file_path, json_file_path):
+    json_array = []
+    # read csv file
+    with open(csv_file_path, encoding='utf-8') as csvf: 
+        # load csv file data using csv library's dictionary reader
+        csv_reader = csv.DictReader(csvf) 
+
+        # convert each csv row into python dict
+        for row in csv_reader: 
+            # add this python dict to json array
+            json_array.append(row)
+    # convert python jsonArray to JSON String and write to file
+    with open(json_file_path, 'w', encoding='utf-8') as json_file: 
+        json_string = json.dumps(json_array, indent=4)
+        json_file.write(json_string)
+
+for root_dir, cur_dir, files in os.walk('./data'):
+    csv_files = list(filter(lambda file: '.csv' in file, files))
+    if len(csv_files) == 0:
+        continue
+    for file in files:
+        csv_file_path = root_dir + "/" + file
+        json_file_path = csv_file_path.replace("./data", "./json").replace(".csv", ".json")
+        csv_to_json(csv_file_path, json_file_path)
